1ed35754ede4fe22fe831c17c5779a7aa35fa1ad
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-12-22 14:42:58 -0500

    Update views.py

--- redirect/views.py
+++ redirect/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponseRedirect
 
 def redirect_view(request):
-    return HttpResponseRedirect('https://github.com/ykdojo/editdojo')
+    return HttpResponseRedirect('https://www.csdojo.io/edit')
